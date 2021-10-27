e42d1516eec2105bc6a93297c542749230e5aa4e
Milan Manigoda <bestsoftdev2020@yandex.ru>
2021-10-28 02:45:08 +0300

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $project_title = '| Viho - Premium Admin Template';
+        View::share('title', $project_title);
+
+        Schema::defaultStringLength(191);
+    }
+}
